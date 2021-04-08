07a52f8984aac6745fa9bbdd04638ba274844632
Zachary Ruffner <locutus1986@gmail.com>
2021-04-07 22:27:28 -0400

    Redux set up and SongList

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        {title: 'walk the line', duration: '3.45'},
+        {title: 'first day of my life', duration: '6.25'},
+        {title: 'the flying Dutchmen', duration: '9.59'},
+        {title: '7 & 7s', duration: '3.48'},
+    ]
+}
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if(action.type === 'SONG_SELECTION'){
+        return action.payload;
+    }
+
+    return selectedSong
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
